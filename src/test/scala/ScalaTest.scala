de65f18f80eaa186850ac06770b8cc2f1d0fcd3d
liushuai <liushuai@shhxzq.com>
2017-08-11 19:01:00 +0800

    实时计算-6-从Mysql取出数据到streaming

--- src/test/scala/ScalaTest.scala
+++ src/test/scala/ScalaTest.scala
@@ -5,13 +5,17 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object ScalaTest {
   //数据库配置
-  lazy val url = "jdbc:mysql://your_ip:3306/my_test"
-  lazy val username = "root"
-  lazy val password = "secret_password"
+//  lazy val url = "jdbc:mysql://your_ip:3306/my_test"
+//  lazy val username = "root"
+//  lazy val password = "secret_password"
+
+  lazy val url = "jdbc:mysql://10.199.107.240:5508/om"
+  lazy val username = "crm"
+  lazy val password = "crm"
 
   def main(args: Array[String]) {
     //    val sparkConf = new SparkConf().setAppName("sparkSqlTest").setMaster("local[2]").set("spark.app.id", "sql")
-    val sparkConf = new SparkConf().setAppName("sparkSqlTest").setMaster("yarn-cluster").set("spark.app.id", "sqlTest")
+    val sparkConf = new SparkConf().setAppName("sparkSqlTest").setMaster("local[2]")
     //序列化
     sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     sparkConf.set("spark.kryoserializer.buffer", "256m")
@@ -32,25 +36,25 @@ object ScalaTest {
     //注意：集群上运行时，一定要添加这句话，否则会报找不到mysql驱动的错误
     prop.put("driver", "com.mysql.jdbc.Driver")
     //加载mysql数据表
-    val df_test1: DataFrame = sqlContext.read.jdbc(uri, "user_t", prop)
-    val df_test2: DataFrame = sqlContext.read.jdbc(uri, "t_user2", prop)
+//    val df_test1: DataFrame = sqlContext.read.jdbc(uri, "user_t", prop)
+    val df_test2: DataFrame = sqlContext.read.jdbc(uri, "dm_liushuai_sstreaming_test", prop)
 
     //从dataframe中获取所需字段
-    df_test2.select("id", "name", "age").collect()
+    df_test2.select("id", "click_count", "status").collect()
       .foreach(row => {
-        println("id  " + row(0) + " ,name  " + row(1) + ", age  " + row(2))
+        println("id  " + row(0) + " ,click_count  " + row(1) + ", status  " + row(2))
       })
     //注册成临时表
-    df_test1.registerTempTable("temp_table")
-
-    val total_sql = "select * from temp_table "
-    val total_df: DataFrame = sqlContext.sql(total_sql)
-
-    //将结果写入数据库中
-    val properties=new Properties()
-    properties.setProperty("user","root")
-    properties.setProperty("password","secret_password")
-    total_df.write.mode("append").jdbc("jdbc:mysql://your_ip:3306/my_test?useUnicode=true&characterEncoding=UTF-8","t_result",properties)
+//    df_test1.registerTempTable("temp_table")
+//
+//    val total_sql = "select * from temp_table "
+//    val total_df: DataFrame = sqlContext.sql(total_sql)
+//
+//    //将结果写入数据库中
+//    val properties=new Properties()
+//    properties.setProperty("user","root")
+//    properties.setProperty("password","secret_password")
+//    total_df.write.mode("append").jdbc("jdbc:mysql://your_ip:3306/my_test?useUnicode=true&characterEncoding=UTF-8","t_result",properties)
 
     /**
       * 注意：查看源码可以知道详细意思
@@ -66,13 +70,12 @@ object ScalaTest {
       */
 
     //分组后求平均值
-    total_df.groupBy("name").avg("age").collect().foreach(x => {
-      println("name " + x(0))
-      println("age " + x(1))
-    })
+//    total_df.groupBy("name").avg("age").collect().foreach(x => {
+//      println("name " + x(0))
+//      println("age " + x(1))
+//    })
 
   }
 }
 
 
-逻辑都拉进来
