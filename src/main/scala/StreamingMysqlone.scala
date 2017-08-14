1e8bec37497cb79a07256f56d65c40e8440e7259
liushuai <liushuai@shhxzq.com>
2017-08-14 17:37:52 +0800

    streaming完整代码-08-14-1737

--- src/main/scala/StreamingMysqlone.scala
+++ src/main/scala/StreamingMysqlone.scala
@@ -1,6 +1,9 @@
 
+import java.util.Properties
+
 import com.alibaba.fastjson.JSON
-import org.apache.spark.{HashPartitioner, SparkConf}
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka.KafkaUtils
@@ -8,7 +11,12 @@ import org.apache.spark.streaming.kafka.KafkaUtils
 
 object StreamingMysqlone {
   //  case class PlaneInfo(offset: String)
+  //  连接Mysql配置参数
+  lazy val url = "jdbc:mysql://10.199.107.240:5508/om"
+  lazy val username = "crm"
+  lazy val password = "crm"
 
+  //累计算法
   val updateFunc = (iter: Iterator[(String, Seq[Int], Option[Int])]) => {
     //iter.flatMap(it=>Some(it._2.sum + it._3.getOrElse(0)).map(x=>(it._1,x)))
     iter.flatMap { case (x, y, z) => Some(y.sum + z.getOrElse(0)).map(i => (x, i)) }
@@ -18,8 +26,29 @@ object StreamingMysqlone {
     LoggerLevels.setStreamingLogLevels()
     val Array(zkQuorum, group, topics, numThreads) = args
     val sparkConf = new SparkConf().setAppName("LSKafkaWordCount").setMaster("local[2]")
+    //设置允许多个sparkconf对象运行
+    sparkConf.set("spark.driver.allowMultipleContexts", "true")
+    //获取context
+    val sc = new SparkContext(sparkConf)
+    //获取sqlContext
+    val sqlContext = new SQLContext(sc)
+    //创建jdbc连接信息
+    val uri = url + "?user=" + username + "&password=" + password + "&useUnicode=true&characterEncoding=UTF-8"
+    val prop = new Properties()
+    //注意：集群上运行时，一定要添加这句话，否则会报找不到mysql驱动的错误
+    prop.put("driver", "com.mysql.jdbc.Driver")
+    //加载mysql数据表
+    //    val df_test1: DataFrame = sqlContext.read.jdbc(uri, "user_t", prop)
+    val df_test2: DataFrame = sqlContext.read.jdbc(uri, "dm_sstreaming_getdata_test", prop)
+    //获取最后一行数据
+    val lastData = df_test2.select("insert_time", "click_sum").collect().last
+    val lastClick = lastData.get(1)
+    println("-------------：" + lastClick)
+
+
     val ssc = new StreamingContext(sparkConf, Seconds(5))
     ssc.checkpoint("/Users/liushuai/Desktop/temp/kafkalogs")
+
 
     //"alog-2016-04-16,alog-2016-04-17,alog-2016-04-18"
     val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
@@ -39,7 +68,7 @@ object StreamingMysqlone {
     val valAfter1 = after1.map(x => ("click_Counts", x.getString("value").toInt))
     //    计算点击总数
     val click_Counts = valAfter1.updateStateByKey(updateFunc, new HashPartitioner(ssc.sparkContext.defaultParallelism), true)
-    val mysqldata = 10000
+    //    val mysqldata = 10000
     //插入mysql数据库
     click_Counts.foreachRDD(rdd => {
       rdd.foreachPartition(partitionOfRecords => {
@@ -50,7 +79,7 @@ object StreamingMysqlone {
 
           partitionOfRecords.foreach(record => {
 
-            stmt.addBatch("insert into dm_sstreaming_getdata_test (insert_time,click_sum) values (now()," + record._2 + " + " + mysqldata + ")");
+            stmt.addBatch("insert into dm_sstreaming_getdata_test (insert_time,click_sum) values (now()," + record._2 + " + " + lastClick + ")");
           })
 
           stmt.executeBatch();
